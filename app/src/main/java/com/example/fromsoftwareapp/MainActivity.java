cfd3c07acd7fdd6067ba89946d603f8e2a8da304
JavierDCH12 <javierdelgadoch95@gmail.com>
2024-10-01 01:37:09 +0200

    binding added

--- app/src/main/java/com/example/fromsoftwareapp/MainActivity.java
+++ app/src/main/java/com/example/fromsoftwareapp/MainActivity.java
@@ -8,13 +8,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.fromsoftwareapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
 
     }
 }
